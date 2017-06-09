304718887592bf06585091aa43dd9e99df78de59
Diego Augusto <daugustomoura@gmail.com>
2017-06-08 22:46:08 -0300

    completando o jogo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private int count;
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     private void FixedUpdate()
@@ -18,6 +25,25 @@ public class PlayerController : MonoBehaviour
 
         Vector3 moviment = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(moviment);
+        rb.AddForce(moviment * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 14)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
